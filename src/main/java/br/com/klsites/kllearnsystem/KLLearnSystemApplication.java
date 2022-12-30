7927c123746686849359aa411e2aa99cb5bdcd39
Kleber Martins <kleber@klsites.com.br>
2022-12-30 10:10:52 -0300

    projeto criado

---
+++ src/main/java/br/com/klsites/kllearnsystem/KLLearnSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.klsites.kllearnsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KLLearnSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KLLearnSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/klsites/kllearnsystem/KLLearnSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.klsites.kllearnsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KLLearnSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
